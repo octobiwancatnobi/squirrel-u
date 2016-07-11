b8dbb7d302657ae7bd42169b961f5a343db70326
Jim Kohl <jkohl2@gaig.com>
2016-07-11 14:45:24 -0400

    removed usage of rake for generating hierarchy (to keep things simpler)

--- scripts/runJekyll.sh
+++ scripts/runJekyll.sh
@@ -3,5 +3,13 @@ echo "Updating gem bundle"
 bundle update
 echo "Starting Jekyll, this may take several minutes ..."
 jekyll clean
-rake
+
+# this next section is an optimization to lessen startup time for a larger content collection
+# if you uncomment the line rake command below, you'll need to also make sure that:
+#    if you commit hierarchy-template.html,
+#        copy its contents into hierarchy.html
+#        remove the 3 top lines
+#        commit hierarchy.html
+# rake
+
 jekyll serve --destination _site --incremental --profile
