e66cfacdf842bf446c043b3100270fbc68a49d6d
dben <dben@GAIG.COM>
2016-07-13 11:00:43 -0400

    Updates for issue 13

--- setup/scripts/buildJekyll.sh
+++ setup/scripts/buildJekyll.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+#! /bin/bash
 # Initial setup commands
 rubyCommand=`ruby -v` # check ruby version, this line produces an error and results in an empty string if it is not found
+rvmCommand=`rvm -v` # check rvm version, this line produces an error and results in an empty string if it is not found
 os=`echo $OSTYPE` #check to see what operating system we're on
-if [[ "$rubyCommand" == "" ]]; then
+
+if [[ "$os" == "darwin"* ]]; then
+    #On Macs check for RVM and install if it is not found
+
+    if [[ "$rvmCommand" == "" ]]; then
     #only install ruby if it is not already installed
-    if [[ "$os" == "darwin"* ]]; then
+
         # Mac OSX
         echo "Running Mac setup to download and install Ruby via RVM"
         echo "Downloading Homewbrew ..."
@@ -15,29 +20,40 @@ if [[ "$rubyCommand" == "" ]]; then
         # install rvm; use rvm to get latest version  (enter your password if prompted)
         echo "Downloading and installing rvm ..."
         curl -L https://get.rvm.io | bash -s stable --ruby
+
         source ~/.rvm/scripts/rvm
         rvm list known      # Check all know versions of ruby (pick one from the list)
+        rvm install ruby-2.3.1     # install latest version of Ruby
+    fi
 
-        rvm install 2.2     # install latest version of Ruby
-        rvm use 2.2.1p85    # or use the one that chose from the step above
-        rvm rubygems latest
-    elif [[ "$os" == "cygwin" ]]; then
+    source ~/.rvm/scripts/rvm
+    rvm use 2.3.1    # or use the one that chose from the step above
+    rvm rubygems latest
+
+elif [[ "$os" == "cygwin" ]]; then
+    #only install ruby if it is not already installed
+        if [[ "$rvmCommand" == "" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
         echo "Running cygwin setup to download and install Ruby via rvm"
         command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 
         # install rvm; use rvm to get latest version  (enter your password if prompted)
         curl -L https://get.rvm.io | bash -s stable --ruby
+
         source ~/.rvm/scripts/rvm
-
         rvm list known      # Check all know versions of ruby (pick one from the list)
+        rvm install ruby-2.3.1     # install latest version of Ruby
+    fi
 
-        rvm install ruby-2.2.1p85 # install latest version of Ruby
-        rvm use 2.2.1p85    # or use the one that chose from the step above
-        rvm rubygems latest
-    elif [[ "$os" == "msys" ]]; then
+    source ~/.rvm/scripts/rvm
+    rvm use 2.3.1    # or use the one that chose from the step above
+    rvm rubygems latest
+
+elif [[ "$os" == "msys" ]]; then
+    #only install ruby if it is not already installed
+    if [[ "$rubyCommand" == "" ]]; then
         echo "Running Windows setup to download and install Ruby"
-        # If command parameters did work this is what we'd want to use: ./rubyinstaller-2.2.1-x64 /verysilent /dir="c/Development/Ruby" /tasks="modpath"
+        # If command parameters did work this is what we'd want to use: ./rubyinstaller-2.3.0-x64 /verysilent /dir="c/Development/Ruby" /tasks="modpath"
         echo "################################################################################"
         echo "# Downloading Ruby Installer ...                                               #"
         echo "#                                                                              #"
@@ -45,7 +61,7 @@ if [[ "$rubyCommand" == "" ]]; then
         echo "# * Check the checkbox to 'Add Ruby Executables to your PATH'                  #"
         echo "################################################################################"
         echo ""
-        mkdir -p /c/Development/Ruby && (cd /c/Development/Ruby; curl -L http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.2.1-x64.exe -o rubyinstaller-2.2.1-x64.exe && ./rubyinstaller-2.2.1-x64)
+        mkdir -p /c/Development/Ruby && (cd /c/Development/Ruby; curl -L http://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.3.0-x64.exe -o rubyinstaller-2.3.0-x64.exe && ./rubyinstaller-2.3.0-x64)
 
         echo ""
         echo "################################################################################"
@@ -67,9 +83,11 @@ if [[ "$rubyCommand" == "" ]]; then
         echo "############      Restart IntelliJ and re-run this same setup!      ############"
         read
         exit
+    else
+        echo "Ruby is already installed, continuing with gem installs ..."
     fi
 else
-    echo "Ruby is already installed, continuing with gem installs ..."
+    echo "Unknown OS, continuing with gem installs ..."
 fi
 
 if [[ "$os" == "msys" ]]; then
@@ -81,16 +99,12 @@ if [[ "$os" == "msys" ]]; then
     here=`pwd`
     ruby_dev_kit_install="$here/scripts/ruby_dev_kit_install.bat"
     $ruby_dev_kit_install =~ s{/}{\\}g;
-    # install jekyll, jemoji and jekyll-lunr-search gems
-    gem dependency -p http://squid.td.afg:3128/
 else
     # Copy run configurations and other files over
     cp -rv setup/idea-init/workspace/* .idea #will overwrite the workspace.xml, this is intentional
     cp -rnv setup/idea-init/other/* .idea   #will not overwrite files
 
     # install jekyll, jemoji and jekyll-lunr-search gems
-    gem dependency -p http://squid.td.afg:3128/
-
     #cannot be installed on Windows
     gem install jekyll-lunr-js-search
 fi
@@ -104,11 +118,10 @@ bundle update
 # setup upstream remote; origin already set
 upExist=`git remote|grep upstream`
 if [ ! "$upExist" == "upstream" ]; then
-    git remote add upstream https://github.gaig.com/BUE/CubeU
+    git remote add upstream https://github.com/zippyzsquirrel/squirrel-u
     git fetch --all
     git branch -u upstream/gh-pages
-    git remote set-url --push upstream no_push
-    echo "CUBE U upstream remote added"
+    echo "Squirrel U upstream remote added"
 else
-    echo "CUBE U upstream remote already exists ..."
+    echo "Squirrel U upstream remote already exists ..."
 fi
