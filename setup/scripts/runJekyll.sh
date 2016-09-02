c66c4e8707d28dd6ec84c1d66f97ffb42cbc981e
Jim Kohl <jkohl2@gaig.com>
2016-09-02 11:19:10 -0400

    removed comment remnant from old way of preparing hierarchy.

--- setup/scripts/runJekyll.sh
+++ setup/scripts/runJekyll.sh
@@ -15,12 +15,4 @@ bundle update
 echo "Starting Jekyll, this may take several minutes ..."
 jekyll clean
 
-# this next section is an optimization to lessen startup time for a larger content collection
-# if you uncomment the line rake command below, you'll need to also make sure that:
-#    if you commit hierarchy-template.html,
-#        copy its contents into hierarchy.html
-#        remove the 3 top lines
-#        commit hierarchy.html
-# rake
-
 jekyll serve --destination _site --incremental --profile
